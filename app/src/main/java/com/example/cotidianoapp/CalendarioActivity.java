2f3c212bb3ac9e9f63959aaac79586bc5448b03b
BryanAlfaroMondaca1 <bryan.alfaro04@inacapmail.cl>
2024-09-24 22:33:38 -0300

    final

--- app/src/main/java/com/example/cotidianoapp/CalendarioActivity.java
+++ app/src/main/java/com/example/cotidianoapp/CalendarioActivity.java
@@ -2,7 +2,7 @@ package com.example.cotidianoapp;
 
 import android.os.Bundle;
 import android.widget.CalendarView;
-import android.widget.Toast;
+import android.widget.Toast; 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CalendarioActivity extends AppCompatActivity {
